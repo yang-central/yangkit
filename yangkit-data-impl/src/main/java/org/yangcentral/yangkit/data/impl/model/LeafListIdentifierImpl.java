6a08ffee98615b97e7a13d8b50ed3533a30e0ba1
frank feng <frank.fengchong@huawei.com>
2023-06-27 14:52:49 +0800

    fix bug for leaf-list identifier

--- yangkit-data-impl/src/main/java/org/yangcentral/yangkit/data/impl/model/LeafListIdentifierImpl.java
+++ yangkit-data-impl/src/main/java/org/yangcentral/yangkit/data/impl/model/LeafListIdentifierImpl.java
@@ -3,6 +3,8 @@ package org.yangcentral.yangkit.data.impl.model;
 import org.yangcentral.yangkit.common.api.QName;
 import org.yangcentral.yangkit.data.api.model.LeafListIdentifier;
 
+import java.util.Objects;
+
 public class LeafListIdentifierImpl extends DataIdentifierImpl implements LeafListIdentifier {
     String value;
     public LeafListIdentifierImpl(QName qName,String value) {
@@ -13,5 +15,26 @@ public class LeafListIdentifierImpl extends DataIdentifierImpl implements LeafLi
     @Override
     public String getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        LeafListIdentifierImpl that = (LeafListIdentifierImpl) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), value);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(getQName().getQualifiedName());
+        sb.append("{").append("value='" + value + '\'').append("}");
+        return sb.toString();
     }
 }
