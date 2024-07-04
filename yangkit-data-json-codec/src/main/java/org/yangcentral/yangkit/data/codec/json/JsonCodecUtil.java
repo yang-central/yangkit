5c6d7549c9bc92abaff8ad17851a835f56b66ff6
BVivek974 <vivekananda.boudia@gmail.com>
2024-07-04 12:34:50 +0200

    fix error path

--- yangkit-data-json-codec/src/main/java/org/yangcentral/yangkit/data/codec/json/JsonCodecUtil.java
+++ yangkit-data-json-codec/src/main/java/org/yangcentral/yangkit/data/codec/json/JsonCodecUtil.java
@@ -605,7 +605,7 @@ public class JsonCodecUtil {
             ValidatorRecordBuilder<String, JsonNode> recordBuilder = new ValidatorRecordBuilder<>();
             JsonNode tempJsonNode = (JsonNode) record.getBadElement();
             recordBuilder.setErrorTag(record.getErrorTag());
-            recordBuilder.setErrorPath(extraValidationData.getJsonPath(tempJsonNode));
+            recordBuilder.setErrorPath(record.getErrorPath() != null ? record.getErrorPath().toString() : extraValidationData.getJsonPath(tempJsonNode));
             recordBuilder.setBadElement(tempJsonNode);
             recordBuilder.setErrorMessage(record.getErrorMsg());
             validatorResultBuilderWithErrorPath.addRecord(recordBuilder.build());
